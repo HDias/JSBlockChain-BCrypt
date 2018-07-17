48adee7831673b321aa8eed05aab9e83bd81b3c0
Horecio Araújo Dias <horecio@gmail.com>
2018-07-17 00:10:06 -0300

    Minering package.json

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-// alterar para https://www.npmjs.com/package/bcrypt
-// const SHA256 = require('crypto-js/sha256');
+/**
+ * npm install --save crypto-js
+ * npm install bcrypt --save 
+ */
+const SHA256 = require('crypto-js/sha256');
 var bcrypt = require('bcrypt');
 
-//https://www.youtube.com/watch?v=zVqczFZr124&t=801s
+// https://www.youtube.com/watch?v=zVqczFZr124&t=801s
+// https://www.youtube.com/watch?v=HneatE69814
+// https://www.youtube.com/watch?v=fRV6cGXVQ4I
 
 class Block {
     /**
@@ -19,23 +24,34 @@ class Block {
         this.previousHash = previousHash;
 
         this.hash = this.calculateHash();
+
+        this.nonce = 0;
     }
 
     calculateHash() {
-        const saltRounds = 10;
-        const myPlaintextPassword = this.dataToCrypt();
-
-        return bcrypt.hashSync(myPlaintextPassword, saltRounds);
+        return SHA256(this.dataToCrypt()).toString();
+        // const saltRounds = 10;
+        // return bcrypt.hashSync(this.dataToCrypt(), saltRounds).toString();
     }
 
     dataToCrypt() {
-        return this.index + this.previousHash + this.timestamp + JSON.stringify((this.data)).toString();
+        return this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce;
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log('Bloco minerado: ' + this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4; // Gera o has com quatro 0 no início
     }
 
     createGenesisBlock() {
@@ -48,7 +64,8 @@ class Blockchain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
+        // newBlock.hash = newBlock.calculateHash();
 
         this.chain.push(newBlock);
     }
@@ -58,7 +75,7 @@ class Blockchain {
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
 
-            if(! bcrypt.compareSync(currentBlock.dataToCrypt(), currentBlock.hash)) {
+            if(currentBlock.hash !==currentBlock.calculateHash()) {
                 return false;
             }
 
@@ -72,13 +89,18 @@ class Blockchain {
 }
 
 let hDiasCoin = new Blockchain();
+
+console.log('Minerando Block 1...');
 hDiasCoin.addBlock(new Block(1, '02/01/2018', {amount: 2}));
+
+console.log('Minerando Block 2...');
 hDiasCoin.addBlock(new Block(2, '03/01/2018', {amount: 5}));
 
-console.log('Blockachain é válido?', hDiasCoin.isChainValid());
+// console.log('Blockachain é válido?', hDiasCoin.isChainValid());
 
-hDiasCoin.chain[1].data = { amount: 100}
+// hDiasCoin.chain[1].data = { amount: 100 }
+// hDiasCoin.chain[1].data = hDiasCoin.chain[1].calculateHash()
 
-console.log('Blockachain é válido?', hDiasCoin.isChainValid());
+// console.log('Blockachain é válido?', hDiasCoin.isChainValid());
 
 // console.log(JSON.stringify(hDiasCoin, null, 2));
